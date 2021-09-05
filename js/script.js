8766136f704323e79b9a27751002bfd5f7e71339
davidson-vs <davidson.vieiras27@gmail.com>
2021-09-05 16:50:56 -0300

    improves on the layout, adding the event mouse enter and leave

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-function scrollMenu(){
-    let menu = document.querySelector("header")
-    menu.classList.toggle("scroll", window.scrollY > 210)
+function scrollMenu() {
+   let menu = document.querySelector("header")
+   menu.classList.toggle("scroll", window.scrollY > 210)
 }
 
-window.addEventListener("scroll",()=>{
-    setInterval(scrollMenu(),3000)
+window.addEventListener("scroll", () => {
+   setInterval(scrollMenu(), 3000)
 })
 
+const itens = document.querySelectorAll(".item")
+const item = document.querySelectorAll(".item")
+
+console.log(item)
+for (tag of itens) {
+   tag.addEventListener("mouseenter", (e) => {
+      e.target.classList.toggle("show")
+   })
+   tag.addEventListener("mouseleave", (e) => {
+      e.target.classList.toggle("show")
+   })
+}
